469b9eba0ac14d76f078c776ebf32fdb5b7e5dc6
Metin Alperen Balta <alperenbalta@atlassian.com>
2023-01-02 23:04:52 +0300

    first commit

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 # .\venv\Scripts\activate
 # python -m flask --app .\app.py run
-from flask import Flask, jsonify, redirect, request
+from flask import Flask, jsonify, redirect, request, abort
 import sqlite3
 import random
 import string
+import re
 from flask import render_template, url_for, redirect
-from flask_sqlalchemy import SQLAlchemy
+import logging
+from flask import request, Response
+# from flask_sqlalchemy import SQLAlchemy
 # from flask_login import UserMixin
 
 app = Flask(__name__)
@@ -50,6 +53,15 @@ users = [
 ]
 
 
+# Create a blacklist of potentially malicious strings
+blacklist = [";", "--", "\"", "\'", ";--", "/*", "*/", "@@",
+                "@", "char", "nchar", "varchar", "nvarchar", "alter",
+                "begin", "cast", "create", "cursor", "declare", "delete",
+                "drop", "end", "exec", "execute", "fetch", "insert",
+                "kill", "open", "select", "sys", "sysobjects", "syscolumns",
+                "table", "update"]
+
+
 @app.route("/")
 def home():
     title = "Welcome"
@@ -57,11 +69,36 @@ def home():
     return render_template('home.html', title=title)
 
 
+
+# set up logging
+logging.basicConfig(filename='request_logs.txt', level=logging.INFO)
+
+@app.before_request
+def log_request_data():
+    """Logs the request data."""
+    logging.info('{} {}'.format(request.method, request.url))
+
+@app.after_request
+def log_response_data(response):
+    """Logs the response data."""
+    logging.info('Response status: {}'.format(response.status_code))
+    logging.info('Response data: {}'.format(response.data))
+    return response
+
+
 @app.route("/api/posts", methods=['GET'])
 def result():
     connection = sqlite3.connect(filepath)
     cursor = connection.cursor()
     post_id = request.args.get("post_id")
+
+    # Validate and sanitize the post_id input
+    if not post_id.isdigit():
+        return "'post_id' must be an integer", 400
+    for pattern in blacklist:
+        if re.search(pattern, post_id):
+            return "Potentially malicious input detected", 400
+                
     if post_id is None:
         query = "SELECT * FROM posts"
         post = cursor.execute(query)
@@ -76,6 +113,8 @@ def result():
     finally:
         connection.close()
     return jsonify(post)
+
+   
 
 
 @app.route('/login', methods=['GET', 'POST'])
